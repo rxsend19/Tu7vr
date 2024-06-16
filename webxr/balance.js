0014255fb7c40558fd911aa7a7126a287e04d5d6
rxsend19 <74374606+rxsend19@users.noreply.github.com>
2024-06-16 15:01:17 +0500

    Update balance.js

--- webxr/balance.js
+++ webxr/balance.js
@@ -1,11 +1,1 @@
-var numm = Math.round(Math.random() * 10)
-console.log(numm)
-if(numm == 3){
-console.log("OK")
-    window.location.href = 'https://admin1911.cloudns.cl/other/kvant/tu7/output/' // balance users 
-}
-if(numm == 9){
-console.log("OK")
-    window.location.href = 'https://admin1911.cloudns.cl/other/kvant/tu7/output/' // balance users 
-}
 
